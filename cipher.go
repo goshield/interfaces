fcdb878a5d3063b637dc8651390c55b793ca3131
Lucas <me@dotronglong.com>
2019-05-29 07:16:43 +0800

    Fix cipher

--- cipher.go
+++ cipher.go
@@ -6,9 +6,9 @@ type Cipherable interface {
 }
 
 type Encryptable interface {
-	encrypt(text string, key string) (value string, err error)
+	Encrypt(text string, key string) (value string, err error)
 }
 
 type Decryptable interface {
-	decrypt(text string, key string) (value string, err error)
+	Decrypt(text string, key string) (value string, err error)
 }
